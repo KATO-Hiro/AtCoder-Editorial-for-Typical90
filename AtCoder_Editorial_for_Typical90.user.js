a313144b03677c70ca03ee32201549502ce6f45a
Kato Hiroki <k.hiro1818@gmail.com>
2021-06-02 15:52:47 +0900

    :book: Fix docs (#20)

--- AtCoder_Editorial_for_Typical90.user.js
+++ AtCoder_Editorial_for_Typical90.user.js
@@ -2,7 +2,7 @@
 // @name         AtCoder Editorial for Typical90
 // @namespace    http://tampermonkey.net/
 // @version      0.1.0
-// @description  AtCoder「競プロ典型 90 問」に解説タブを追加し、E869120さんのGitHubで公開されている解説・想定ソースコードなどを表示します。
+// @description  AtCoder「競プロ典型 90 問」に解説タブを追加し、E869120さんがGitHubで公開されている問題の解説・想定ソースコードなどを表示します。
 // @match        https://atcoder.jp/contests/typical90*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @author       hiro_hiro
@@ -20,7 +20,7 @@
     addEditorialTabToTaskPage();
     addEditorialPage();
 
-    $(".nav-tabs a").click(function() {
+    $(".nav-tabs a").click(function () {
         changeTab($(this));
         hideContentsOfPreviousPage();
 
