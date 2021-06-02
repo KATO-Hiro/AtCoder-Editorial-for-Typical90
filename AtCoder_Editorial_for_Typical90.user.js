11edc58b071cb01d61f7a75753f858c6a4d243ca
Kato Hiroki <k.hiro1818@gmail.com>
2021-06-02 15:49:26 +0900

    :sparkles: Enable to show texts in new tab (#20)

--- AtCoder_Editorial_for_Typical90.user.js
+++ AtCoder_Editorial_for_Typical90.user.js
@@ -2,24 +2,30 @@
 // @name         AtCoder Editorial for Typical90
 // @namespace    http://tampermonkey.net/
 // @version      0.1.0
-// @description  AtCoder「競プロ典型 90 問」に解説タブを追加し、E869120さんのGitHubで公開されている問題の解説・想定ソースコードなどを表示します。
+// @description  AtCoder「競プロ典型 90 問」の問題ページに解説タブを追加し、E869120さんのGitHubで公開されている解説・想定ソースコードなどを表示します。
 // @match        https://atcoder.jp/contests/typical90*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js
-// @require      https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js
 // @author       hiro_hiro
 // @license      CC0
 // @downloadURL
 // @updateURL
 // @supportURL
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    const url = "";
-    addEditorialTabToTaskPage(url);
+    addTabContentStyles();
+    addEditorialTabToTaskPage();
+    addEditorialPage();
+
+    $(".nav-tabs a").click(function() {
+        changeTab($(this));
+        hideContentsOfPreviousPage();
+
+        return false;
+    });
 
     const current_url = window.location.href;
 
@@ -43,10 +49,41 @@
     }
 })();
 
-function addEditorialTabToTaskPage(url) {
+function addTabContentStyles() {
+    const tabContentStyles = `
+        .tabContent {
+            display: none;
+        }
+        .tabContent.active {
+            display: block;
+        }
+    `;
+
+    GM_addStyle(tabContentStyles);
+}
+
+// FIXME: Hard coding is not good.
+function addEditorialTabToTaskPage() {
     // See:
     // https://api.jquery.com/before/
-    $("li.pull-right").before("<li><a href='" + url + "'><span class='glyphicon glyphicon-book' style='margin-right:4px;' aria-hidden='true'></span>解説</a></li>");
+    $("li.pull-right").before("<li><a href='#editorial-created-by-userscript'><span class='glyphicon glyphicon-book' style='margin-right:4px;' aria-hidden='true'></span>解説</a></li>");
+}
+
+function addEditorialPage() {
+    const contestNavTabsId = document.getElementById("contest-nav-tabs");
+
+    // See:
+    // https://stackoverflow.com/questions/268490/jquery-document-createelement-equivalent
+    // https://blog.toshimaru.net/jqueryhidden-inputjquery/
+    $("<div>", {
+        class: "tabContent",
+        id: "editorial-created-by-userscript",
+    }).appendTo(contestNavTabsId);
+
+    $("<h2>", {
+        class: "editorial-header",
+        text: "解説"
+    }).appendTo("#editorial-created-by-userscript");
 }
 
 function addEditorialButtonToTaskPage() {
@@ -64,5 +101,32 @@ function addEditorialButtonToTaskPage() {
         return editorialButton;
     } else {
         return;
+    }
+}
+
+function changeTab(this_object) {
+    // See:
+    // https://api.jquery.com/parent/
+    // https://api.jquery.com/addClass/#addClass-className
+    // https://api.jquery.com/siblings/#siblings-selector
+    // https://api.jquery.com/removeClass/#removeClass-className
+    // https://www.design-memo.com/coding/jquery-tab-change
+    this_object.parent().addClass("active").siblings(".active").removeClass("active");
+    const tabContentsUrl = this_object.attr("href");
+    $(tabContentsUrl).addClass("active").siblings(".active").removeClass("active");
+}
+
+function hideContentsOfPreviousPage() {
+    // See:
+    // https://api.jquery.com/length/
+    // https://api.jquery.com/hide/
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String
+    const tagCount = $(".col-sm-12").length;
+
+    for (let index = 0; index < tagCount; index++) {
+        if (index != 0) {
+            $("#main-container > div.row > div:nth-child(" + String(index + 1) + ")").hide();
+        }
     }
 }
