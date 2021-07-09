0ce0d33636afce6232f615198b63a48679c01727
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-09 19:59:26 +0900

    🐛 Update editorial id 087 (#96)

--- AtCoder_Editorial_for_Typical90.user.js
+++ AtCoder_Editorial_for_Typical90.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtCoder Editorial for Typical90
 // @namespace    https://github.com/KATO-Hiro
-// @version      0.4.1
+// @version      0.4.2
 // @description  AtCoder「競プロ典型 90 問」に解説タブを追加し、E869120さんがGitHubで公開されている問題の解説・想定ソースコードなどのリンクを表示します。
 // @match        https://atcoder.jp/contests/typical90*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
@@ -575,6 +575,7 @@ function getMultipleEditorialUrlsIfNeeds(taskId) {
         "084": ["-01", "-02"],
         "085": ["-01", "-02"],
         "086": ["-01", "-02"],
+        "087": ["-01", "-02"],
     };
 
     if (taskId in multipleEditorialUrls) {
